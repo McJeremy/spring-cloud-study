7c73da42e4b4d6f37e8c4837b3d15e4bfbc2077a
xuzz <ilwm1984.xu@hotmail.com>
2018-03-01 09:31:01 +0800

    init

---
+++ service-hi2/src/main/java/com/xuzz/servicehi2/ServiceHi2Application.java
@@ -0,0 +1,30 @@
+package com.xuzz.servicehi2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceHi2Application {
+
+	@Value("${server.port}")
+	String port;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHi2Application.class, args);
+	}
+
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name)
+	{
+		return "hi "+name+"，i am from port:"+port;
+	}
+
+}
